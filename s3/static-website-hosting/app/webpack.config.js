7b915c49f90ea32c5c39226b455b16d6f330b38d
Hiranyagarbh <hiranyagarbhchoudhary@gmail.com>
2025-07-28 15:32:03 +0530

    Update webpack config

--- s3/static-website-hosting/app/webpack.config.js
+++ s3/static-website-hosting/app/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.[contenthash].js",
     clean: true,
     publicPath: "/",
@@ -35,7 +35,7 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "build"),
     },
     historyApiFallback: true,
     port: 3000,
